9a4a7913c50272b4c9c38a926d07da6840757104
Toma Cristian <i.toma_cristian@yahoo.com>
2022-01-02 19:54:27 +0200

    cotnact forms

---
+++ src/app/contact.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { map } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ContactService {
+  private mailApi = 'https://mailthis.to/i.toma_cristian@yahoo.com'
+
+
+  constructor(private http: HttpClient) { }
+  PostMessage(input: any) {
+    return this.http.post(this.mailApi, input, { responseType: 'text' })
+      .pipe(
+        map(
+          (response) => {
+            if (response) {
+              return response;
+            }else{
+              return null;
+            }
+          },
+          (error: any) => {
+            return error;
+          }
+        )
+      )
+  }
+
+}
--- src/app/home/contact/contact.component.ts
+++ src/app/home/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ContactService } from 'src/app/contact.service';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent {
+  FormData!: FormGroup;
 
-  constructor() {
+  constructor(private builder: FormBuilder, private contact: ContactService) {
 }
 
 ngOnInit(): void {
+  this.FormData = this.builder.group({
+    Fullname: new FormControl('', [Validators.required]),
+    Email: new FormControl('', [Validators.required]),
+    Comment: new FormControl('', [Validators.required])
+  });
 }
+onSubmit(FormData) {
+  console.log(FormData)
+  this.contact.PostMessage(FormData)
+    .subscribe(response => {
+      location.href = 'https://mailthis.to/confirm'
+      console.log(response)
+    }, error => {
+      console.warn(error.responseText)
+      console.log({ error })
+    })
+  }
 }
