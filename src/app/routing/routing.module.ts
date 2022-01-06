05a81d79b7f83588424e67abf3082e557d68b442
Toma Cristian <i.toma_cristian@yahoo.com>
2022-01-06 15:30:13 +0200

    404 pages fix

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -8,6 +8,7 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   {path: 'signin', component: SigninComponent},
+  {path: 'home/home', component: HomeComponent}
 ];
 
 @NgModule({
