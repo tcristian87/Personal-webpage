721888e2c6c6d0839a7ad938f2e5e30fde1b2650
Toma Cristian <i.toma_cristian@yahoo.com>
2022-01-07 18:03:50 +0200

    contact functionality

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -6,14 +6,6 @@ import { HomeComponent } from '../home/home.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-  {path: 'signin', component: SigninComponent},
->>>>>>> 314c00103bdb751107432e2ed0966a435ac32b06
-  {path: 'home', component: HomeComponent}
->>>>>>> 6a0d3047821869e83bbcf9c0291857a221fe4e59
 ];
 
 @NgModule({
